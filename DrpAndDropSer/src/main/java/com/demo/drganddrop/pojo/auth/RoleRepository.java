db6826d5d12bc8fdb3e85df01c11d49571f78672
Babin K (BLR GSS) <babink@nousinfo.com>
2018-02-14 18:31:33 +0530

    Removed folder from repository

--- DrpAndDropSer/src/main/java/com/demo/drganddrop/pojo/auth/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.drganddrop.pojo.auth;
-
-import com.demo.drganddrop.pojo.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
